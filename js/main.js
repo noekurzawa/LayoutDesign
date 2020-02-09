8dc9a71e5bdc5ad779dc60837fbb9f59dae520d9
Noe Kurzawa <noekurzawa@gmail.com>
2020-02-09 13:27:40 +0100

    created menu hamburger

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const menuButton = document.querySelector(".menu");
+const menu = document.querySelector("aside");
+
+function showMenu() {
+  menu.classList.toggle('active');
+
+  if(menu.classList.contains('active')) {
+    menuButton.style.left = '260px';
+  } else {
+    menuButton.style.left = '0';
+  }
+}
+menuButton.addEventListener("click", showMenu);
+
+
+function hideMenu() {
+  menu.classList.toggle("active");
+
+  if (menu.classList.contains("active")) {
+    menu.style.position = "absolute";
+  } else {
+    menu.style.position = "fixed";
+  }
+}
+li.addEventListener("click", hideMenu);
